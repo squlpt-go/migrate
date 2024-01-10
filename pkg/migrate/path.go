ce966c7a0aa6349d05b16b5a81a0d366f1a8c124
Ryan Williams <rwilliams@stream9.net>
2024-01-10 09:24:00 -0500

    Fixed path

--- pkg/migrate/path.go
+++ pkg/migrate/path.go
@@ -9,9 +9,9 @@ import (
 // these funcs stolen from: https://stackoverflow.com/a/70491592/2626654
 
 func CurrentDirname() (string, error) {
-	filename, err := currentFilename()
-	if err != nil {
-		return "", err
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		return "", errors.New("unable to get the current filename")
 	}
 	return filepath.Dir(filename), nil
 }
